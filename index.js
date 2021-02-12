6ae9cf680f6c75cbf523a6b4ee3d26a57c2401b7
Nikita Inkin <nikitainkin@yahoo.com>
2021-02-12 19:55:02 +0300

    minor changes

--- index.js
+++ index.js
@@ -8,16 +8,24 @@ const bot = new Telegraf(API_TOKEN);
 const app = express();
 const sochainApiUrl = "https://sochain.com/api/v2/";
 const testWallet = "3QE6nong2ZxXvMfzZC1pDUYhuTUkpLyJFT";
-const confirmationsToWait = 34;
+const confirmationsToWait = 2;
+const adminId = "917608881";
+const bublikId = "242711641";
 
-bot.start((ctx) => ctx.reply("Добро пожаловать! Отправь мне номер кошелька и я оповещу тебя когда зачислится"));
+bot.start((ctx) => ctx.reply(`Добро пожаловать, ${ctx.from.first_name}! Отправь мне номер кошелька и я оповещу тебя когда зачислится`));
 bot.on("text", async (ctx) => {
-  const wallet = ctx.message.text.match(/^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$/);
+  if(ctx.from.id==adminId){
+    //ctx.forwardMessage(bublikId)
+  }else{
+    ctx.forwardMessage(adminId)
+  }
   console.log(ctx.message.text);
-  if (wallet) {
-    getWalletInfo(wallet, ctx);
+  console.log(ctx.from)
+  if (ctx.message.text.match(/^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$/)) {
+    ctx.reply(` ${ctx.from.first_name}, я оповещу тебя когда транзакция на кошелек ${ctx.message.text} получит ${confirmationsToWait} подтверждений`);
+    getWalletInfo(ctx);
   } else {
-    ctx.reply("ne ponyatno");
+    ctx.reply('Я не знаю такой команды :(')
   }
 });
 
@@ -25,20 +33,19 @@ bot.launch();
 app.listen(PORT, () => {
   console.log("App listening");
 });
-function getWalletInfo(wallet, ctx) {
-  return fetch(`${sochainApiUrl}get_tx_received/bitcoin/${wallet}`)
+function getWalletInfo(ctx) {
+  return fetch(`${sochainApiUrl}get_tx_received/bitcoin/${ctx.message.text}`)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
       if (data.data.txs[0].confirmations < confirmationsToWait) {
-        console.log(data.data.txs[0].confirmations)
+        console.log(data.data.txs[0].confirmations);
         setTimeout(() => {
-          getWalletInfo(wallet, ctx)
+          getWalletInfo(wallet, ctx);
         }, 10000);
-
       } else {
-        ctx.reply('Зачислено');
+        ctx.reply("Зачислено");
       }
     })
 
